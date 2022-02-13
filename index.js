4fc6b66503de3db0a43216826c1dbb2109d44f75
iamajmalsaleem <ajmalsaleem84@gmail.com>
2022-02-13 11:38:49 +0530

    Create MongoDB connection

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.listen(5000, () =>{
-    console.log('Backend server is running! Listening on port 5000');
-})
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successful!"))
+  .catch((err) => console.log(err));
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running! Listening on port 5000");
+});
