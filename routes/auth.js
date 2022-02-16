4cab3fd29bbe43c68fff57a6435d8358dd5cd03e
iamajmalsaleem <ajmalsaleem84@gmail.com>
2022-02-16 12:25:54 +0530

    Setup authRoute (part 2 - CryptoJS)

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 //REGISTER
 
@@ -7,7 +8,10 @@ router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
   });
   try {
     const savedUser = await newUser.save();
