e24150b705735c1052ba393ecc0c93e77cdf85a4
iamajmalsaleem <ajmalsaleem84@gmail.com>
2022-02-16 11:59:33 +0530

    Setup authRoute (part 1)

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
 const userRoute = require("./routes/user");
+const authRoute = require('./routes/auth');
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DB Connection Successful!"))
   .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use('/api/auth', authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running! Listening on port 5000");
