c41c9ee786fec36bdebfc5e1c09067c615d3f6f7
iamajmalsaleem <ajmalsaleem84@gmail.com>
2022-02-22 11:14:38 +0530

    Setup userRoute (part 2 - Delete User)

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 
 //UPDATE
+
 router.put("/:id", verifyTokenAndAuthorize, async (req, res) => {
   if (req.body.password) {
     req.body.password = CryptoJS.AES.encrypt(
@@ -19,6 +20,17 @@ router.put("/:id", verifyTokenAndAuthorize, async (req, res) => {
     );
 
     res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DELETE
+
+router.delete("/:id", verifyTokenAndAuthorize, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted...");
   } catch (err) {
     res.status(500).json(err);
   }
