980c966087f640d12ae95c7aa1fd0cec01cabfea
iamajmalsaleem <ajmalsaleem84@gmail.com>
2022-03-01 11:27:51 +0530

    Setup userRoute (part 5 - Add USER STATS with mongoDB aggregation)

--- routes/user.js
+++ routes/user.js
@@ -67,4 +67,31 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
+//GET USER STATS
+
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          monthNum: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$monthNum",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
