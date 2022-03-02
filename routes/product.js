a8d27ae3a77aecdf7021a388c126fba70b53675a
iamajmalsaleem <ajmalsaleem84@gmail.com>
2022-03-02 16:57:47 +0530

    Setup productRoute (part 2 - UPDATE, DELETE & GET  Product)

--- routes/product.js
+++ routes/product.js
@@ -20,91 +20,68 @@ router.post("/", verifyTokenAndAdmin, async (req, res) => {
   }
 });
 
-//   //UPDATE
+//UPDATE
 
-//   router.put("/:id", verifyTokenAndAuthorize, async (req, res) => {
-//     if (req.body.password) {
-//       req.body.password = CryptoJS.AES.encrypt(
-//         req.body.password,
-//         process.env.PASS_SEC
-//       ).toString();
-//     }
-//     try {
-//       const updatedUser = await User.findByIdAndUpdate(
-//         req.params.id,
-//         { $set: req.body },
-//         { new: true }
-//       );
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
 
-//       res.status(200).json(updatedUser);
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
+    res.status(200).json(updatedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-//   //DELETE
+//DELETE
 
-//   router.delete("/:id", verifyTokenAndAuthorize, async (req, res) => {
-//     try {
-//       await User.findByIdAndDelete(req.params.id);
-//       res.status(200).json("User has been deleted...");
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+    res.status(200).json("Product has been deleted...");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-//   //GET USER
+//GET PRODUCT
 
-//   router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
-//     try {
-//       const user = await User.findById(req.params.id);
+router.get("/find/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    return res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-//       const { password, ...others } = user._doc;
-//       return res.status(200).json(others);
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
+//GET ALL PRODUCTS
 
-//   //GET ALL USERS
+router.get("/", async (req, res) => {
+  const qNew = req.query.new;
+  const qCategory = req.query.category;
+  try {
+    let products;
 
-//   router.get("/", verifyTokenAndAdmin, async (req, res) => {
-//     const query = req.query.new;
-//     try {
-//       const users = query
-//         ? await User.find().sort({ _id: -1 }).limit(5)
-//         : await User.find();
-//       return res.status(200).json(users);
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
+    if (qNew) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(5);
+    } else if (qCategory) {
+      products = await Product.find({
+        categories: {
+          $in: [qCategory],
+        },
+      });
+    } else {
+      products = await Product.find();
+    }
 
-//   //GET USER STATS
-
-//   router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
-//     const date = new Date();
-//     const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
-
-//     try {
-//       const data = await User.aggregate([
-//         { $match: { createdAt: { $gte: lastYear } } },
-//         {
-//           $project: {
-//             monthNum: { $month: "$createdAt" },
-//           },
-//         },
-//         {
-//           $group: {
-//             _id: "$monthNum",
-//             total: { $sum: 1 },
-//           },
-//         },
-//       ]);
-//       res.status(200).json(data);
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
+    return res.status(200).json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
