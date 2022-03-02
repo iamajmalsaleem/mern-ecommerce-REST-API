c9c4e953ab767f53b7a4fb91bd52dda295f30420
iamajmalsaleem <ajmalsaleem84@gmail.com>
2022-03-02 15:46:08 +0530

    Setup productRoute (part 1 - Add Product)

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 require("dotenv").config();
 const userRoute = require("./routes/user");
-const authRoute = require('./routes/auth');
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -11,8 +12,9 @@ mongoose
   .catch((err) => console.log(err));
 
 app.use(express.json());
-app.use('/api/auth', authRoute);
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running! Listening on port 5000");
